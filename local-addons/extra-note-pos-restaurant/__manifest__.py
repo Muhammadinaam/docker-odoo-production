75a98fe188f01711fc3d8fe2dc4dd7991224a0fd
Muhammad Inaam Munir <minaammunir@gmail.com>
2022-10-26 06:17:20 +0500

    added extra note button and removed delete button

--- local-addons/extra-note-pos-restaurant/__manifest__.py
+++ local-addons/extra-note-pos-restaurant/__manifest__.py
@@ -16,11 +16,11 @@
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Uncategorized',
-    'version': '0.1',
+    'category': 'Point of Sale',
+    'version': '15.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': ['pos_restaurant', 'point_of_sale'],
 
     # always loaded
     'data': [
@@ -32,4 +32,12 @@
     'demo': [
         'demo/demo.xml',
     ],
+    'assets': {
+        'point_of_sale.assets': [
+            'extra-note-pos-restaurant/static/src/js/extranote_button.js',
+        ],
+        'web.assets_qweb': [
+            'extra-note-pos-restaurant/static/src/xml/**/*',
+        ],
+    },
 }
--- local-addons/extra-note-pos-restaurant/models/models.py
+++ local-addons/extra-note-pos-restaurant/models/models.py
@@ -1,18 +1,9 @@
 # -*- coding: utf-8 -*-
 
-# from odoo import models, fields, api
+from odoo import models, fields, api
 
 
-# class extra-note-pos-restaurant(models.Model):
-#     _name = 'extra-note-pos-restaurant.extra-note-pos-restaurant'
-#     _description = 'extra-note-pos-restaurant.extra-note-pos-restaurant'
+class PosOrder(models.Model):
+    _inherit = 'pos.order'
 
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         for record in self:
-#             record.value2 = float(record.value) / 100
+    extra_note = fields.Char(string="Extra Note")
